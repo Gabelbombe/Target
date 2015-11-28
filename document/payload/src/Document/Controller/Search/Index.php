6390ed6f221c893c721f77d1904fd9df36219632
ehime <dodomeki@gmail.com>
2015-11-27 17:37:18 -0800

    Indexed search

--- document/payload/src/Document/Controller/Search/Index.php
+++ document/payload/src/Document/Controller/Search/Index.php
@@ -1,8 +1,46 @@
 <?php
 Namespace Document\Controller\Search
 {
+    USE Document\Model\Loader;
+
     Class Index Extends Loader
     {
+        private $params = [],
+            $count  = 0;
 
+        public function __construct($params)
+        {
+            $this->params = $params;
+            $this->load($this->params['file']);
+            $this->strip();
+
+            if (isset($this->params['file']) && ! empty($this->params['strip']))
+            {
+                $this->strip(explode(',', $this->params['strip']));
+            }
+
+            if (isset($this->params['common']))
+            {
+                $this->removeCommonWords();
+            }
+
+            $this->matches();
+
+            echo "\n{$this->count} matches of '{$this->params['find']}' found.'\n\n";
+
+            echo $this->text ."\n";
+
+            exit;
+        }
+
+        public function matches()
+        {
+            $index = array_count_values(explode(' ', $this->text));
+
+            if (isset($index[$this->params['find']])) 
+            {
+                $this->count = $index[$this->params['find']];
+            }
+        }
     }
 }
